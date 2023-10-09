3f3d67452db55bc0430c5d6459797aa64b596e35
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-10-09 19:09:25 +0900

    #1 - 스프링부트 프로젝트 시작

---
+++ src/test/java/com/example/open_api_est/OpenApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.open_api_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
